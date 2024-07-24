4dece75c3865d1da3f6708757d2b2be6d9c488c7
Saurabh Pundir <75524401+saurabhpundir05@users.noreply.github.com>
2024-07-24 17:47:53 +0530

    Add files via upload

---
+++ api/model/user.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+const userSchema=new mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    username:String,
+    password:String,
+    phone:Number,
+    email:String,
+    userType:String
+})
+module.exports=mongoose.model('user',userSchema)
